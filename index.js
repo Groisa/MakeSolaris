a4d185ae63dff9f54fe9a47a73b52577ea5312fe
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-02-23 20:12:08 -0300

    correção mobile

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function fecharmodal() {
 document.getElementById("input-pesq").addEventListener("change", pesquisarInput )
 function pesquisarInput(evento) {
     const btnPesq = evento.target.value
-    if(btnPesq == "perfumaria" || btnPesq == "perfume" ){
+    if(btnPesq == "perfumaria" || btnPesq == "perfume"|| btnPesq == "Perfumaria" || btnPesq == "Perfume" ){
         return window.location.replace("Perfumaria.html")
-    } else if(btnPesq == "maquiagem" || btnPesq == "maquiagens" || btnPesq == "make")  {
+    } else if(btnPesq == "maquiagem" || btnPesq == "maquiagens" || btnPesq == "make" || btnPesq == "Make"|| btnPesq == "Maquiagem" || btnPesq == "Maquiagens")  {
                 return window.location.replace("Maquiagem.html")
-    }else if (btnPesq == "cabelo" || btnPesq == "meu cabelo" || btnPesq == "cabeleira") {
+    }else if (btnPesq == "cabelo" || btnPesq == "meu cabelo" || btnPesq == "cabeleira" || btnPesq == "Cabeleira" || btnPesq == "Cabelo" || btnPesq == "Meu cabelo" || btnPesq == "Meu Cabelo") {
                 return window.location.replace("Meu-cabelo.html") 
  }  
 document.getElementById("btn-pesq").addEventListener("click", pesquisarBtn )
@@ -28,3 +28,17 @@ document.getElementById("btn-pesq").addEventListener("click", pesquisarBtn )
  function pesquisarBtn() {
     pesquisarInput() }
  }
+// abrir e fechar menu //
+
+function opencart() {
+    document.querySelector(".carrinho-compra").style.display = "block"
+} 
+function closecart() {
+    document.querySelector(".carrinho-compra").style.display = "none"
+}
+function openlike() {
+    document.querySelector(".eu-amei").style.display = "block"
+}
+function closelike() {
+    document.querySelector(".eu-amei").style.display = "none"
+}

0b16215339ef303827638f3886c23559b759165c
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-02-22 22:26:31 -0300

    novas abas-funçãopesquisar

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ function abrirmenu() {
 function fecharmenu() {
     document.querySelector(`#menu`).style.display = "none";
 }
+function abrirmodal() {
+    document.querySelector(`#modal-sombra`).style.display = "block"
+}
+function fecharmodal() {
+    document.querySelector(`#modal-sombra`).style.display = "none"
+}
+// pesquisar//
+
+document.getElementById("input-pesq").addEventListener("change", pesquisarInput )
+function pesquisarInput(evento) {
+    const btnPesq = evento.target.value
+    if(btnPesq == "perfumaria" || btnPesq == "perfume" ){
+        return window.location.replace("http://127.0.0.1:5500/Perfumaria.html")
+    } else if(btnPesq == "maquiagem" || btnPesq == "maquiagens" || btnPesq == "make")  {
+                return window.location.replace("http://127.0.0.1:5500/Maquiagem.html")
+    }else if (btnPesq == "cabelo" || btnPesq == "meu cabelo" || btnPesq == "cabeleira") {
+                return window.location.replace("http://127.0.0.1:5500/Meu-cabelo.html") 
+ }  
+document.getElementById("btn-pesq").addEventListener("click", pesquisarBtn )
+
+ function pesquisarBtn() {
+    pesquisarInput() }
+ }
